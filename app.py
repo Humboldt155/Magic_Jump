f956205d3897d4387034adc02dd88b0e507c0f26
Humboldt155 <petr_kozyrev@icloud.com>
2018-05-15 23:59:41 +0300

    4

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
 app = Flask(__name__)
 
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/similar')
+def main_similar():
+    return render_template(
+        'similar.html', product='')
 
+
+@app.route('/similar/<string:product>/')
+def get_similar(product):
+    return render_template(
+        'similar.html', product=product)
 
 if __name__ == '__main__':
     app.run()
