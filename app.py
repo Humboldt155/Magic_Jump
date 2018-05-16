9c983a3eaa87b52e6b2e5ba43acd86bba43d5b41
Humboldt155 <petr_kozyrev@icloud.com>
2018-05-17 00:23:33 +0300

    9

--- app.py
+++ app.py
@@ -10,7 +10,19 @@ code_model_name = pd.read_excel('library/code_model_name.xlsx',
                                        'name'])
 
 code_model_name['product'] = code_model_name['product'].astype(str)
-model = Word2Vec.load('models/word2vec/w2v_mymodel_33')
+
+model_num = 1  # Какую из моделей использовать
+
+similar_qty = 10  # Количество похожих товаров
+
+models_list = [
+    'models/word2vec/w2v_mymodel_33',
+    'models/word2vec/w2v_mymodel_33_min50_sg0_i220_window5_size300',
+    'models/word2vec/w2v_mymodel_33_min50_sg1_i220_window5_size300',
+    'models/word2vec/w2v_mymodel_33_min1000_sg0_i200_window5_size300'
+]
+
+model = Word2Vec.load(models_list[model_num])
 
 
 app = Flask(__name__)
@@ -25,12 +37,13 @@ def convert_df(df: pd.DataFrame):
         products = []
         for index, row in prods.iterrows():
             code = row[0]
-            probty = row[1]
+            probty = round(row[1] * 100, 10)
             name = row[3]
             products.append({'code': code, 'name': name, 'probability': probty})
-        result.append({'model': model, 'products': products[0:6]})
+        result.append({'model': model, 'products': products[0:8]})
 
     return result
+
 
 def convert_df_rel(df: pd.DataFrame, pr_remove):
     current_model = list(code_model_name[code_model_name['product'] == str(pr_remove)]['model_adeo'])[0]
@@ -44,26 +57,41 @@ def convert_df_rel(df: pd.DataFrame, pr_remove):
         products = []
         for index, row in prods.iterrows():
             code = row[0]
-            probty = row[1]
+            probty = row[1] * 100
             name = row[3]
             products.append({'code': code, 'name': name, 'probability': probty})
-        result.append({'model': model, 'products': products[0:6]})
+        result.append({'model': model, 'products': products[0:5]})
 
     return result
 
-@app.route('/similar')
-def main_similar():
-    similar_products = convert_df(get_similar(str(12317232)))
-    return render_template(
-        'similar.html', **locals())
+# @app.route('/similar')
+# def main_similar():
+#     similar_products = convert_df(get_similar(str(12317232)))
+#     similar_products = []
+#     return render_template(
+#         'similar.html', **locals())
 
 
 @app.route('/similar/<string:product>/')
 def get_similar(product):
-    similar_products = convert_df(get_similar(str(product), num=8, same_model=True))
+    name = list(code_model_name[code_model_name['product'] == str(product)]['name'])[0]
+    similar_products = convert_df(get_similar(str(product), num=similar_qty, same_model=True))
     relative_products = convert_df_rel(get_similar(str(product), num=200, same_model=False), product)
     return render_template(
         'similar.html', **locals())
+
+
+@app.route('/predict/<string:product>/')
+def get_predicted(product):
+    codes_list = product.split(',')
+    names = []
+    for p in codes_list:
+        code = list(code_model_name[code_model_name['product'] == str(p)]['product'])[0]
+        name = list(code_model_name[code_model_name['product'] == str(p)]['name'])[0]
+        names.append(str(code) + ' - ' + str(name))
+    similar_products = convert_df(get_predicted(codes_list, num_codes=similar_qty, num_models=True))
+    return render_template(
+        'predict.html', **locals())
 
 
 if __name__ == '__main__':
@@ -105,3 +133,62 @@ def get_similar(product: str, num = 5, same_model = True):
     similars_df = similars_df.head(num)
 
     return similars_df
+
+
+#%% Предсказать покупки
+
+def get_predicted(products: list, num_codes = 5, num_models = 20, remove_used_models = True):
+    """Получить список похожих товаров.
+    Parameters:
+        products(str): Код продукта
+        num_codes(int): Топ моделей
+        num_models(int): Топ артикулов в каждой модели
+        remove_used_models(bool): Не показывать модели, которые были в запросе
+    Возвращает:
+        predicted (list): список таблиц pd.DataFrame
+    """
+
+    analogs = pd.DataFrame(columns=['product', 'probability', 'model_adeo', 'name'])
+
+    predicted = model.predict_output_word(products, topn=num_codes*num_models*5)
+    predicted = pd.DataFrame(predicted, columns=['product', 'probability'])
+
+    similars = []
+    for product in products:
+        similars.append(list(analogs.append(get_similar(product, num=2, same_model=True))['product']))
+
+    for s in similars:
+        for p in s:
+            pred = model.predict_output_word([str(p)], topn=num_codes*num_models*5)
+            pred = pd.DataFrame(pred, columns=['product', 'probability'])
+            pred = pred[pred['product'] != p]
+            predicted = predicted.append(pred)
+
+
+    predicted = predicted.sort_values(by='probability', ascending=False)
+
+    # Удаляем позиции, которые в запросе
+    for product in products:
+        predicted = predicted[predicted['product'] != product]
+
+    # Подтягиваем модель и название
+    predicted = predicted.merge(code_model_name, on='product')
+
+    if remove_used_models:
+        current_models = []
+        for product in products:
+            current_models.append(list(code_model_name[code_model_name['product'] == str(product)]['model_adeo'])[0])
+        print(current_models)
+        for model_adeo in current_models:
+            predicted = predicted[predicted['model_adeo'] != model_adeo]
+
+    predicted = predicted.sort_values(by='probability', ascending=False)
+    #
+    # all_models = list(pd.DataFrame(predicted['model_adeo'].unique())[0])
+    #
+    # result = {}
+    #
+    # for mod in range(0, num_models):
+    #     result[all_models[mod]] = predicted[predicted['model_adeo'] == all_models[mod]].head(num_codes)
+
+    return predicted
