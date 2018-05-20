8332b414f6702d10ae38a085cc282ed8bac50ba9
Humboldt155 <petr_kozyrev@icloud.com>
2018-05-20 22:12:38 +0300

    125

--- app.py
+++ app.py
@@ -6,17 +6,22 @@ from gensim.models import Word2Vec
 import pandas as pd
 from flask_cors import CORS
 
-model_num = 1  # Какую из моделей использовать
+model_num = 0 # Какую из моделей использовать
 
 similar_qty = 10  # Количество похожих товаров
 
 models_list = [
-    'models/word2vec/w2v_mymodel_33',
-    'models/word2vec/w2v_mymodel_33_min50_sg0_i220_window5_size300',
-    'models/word2vec/w2v_mymodel_33_min50_sg1_i220_window5_size300',
-    'models/word2vec/w2v_mymodel_33_min1000_sg0_i200_window5_size300',
-    'models/word2vec/w2v_mymodel_33_min5_sg0_i250_window3_size300_transSplit'
+    'models/word2vec/w2v_mymodel_33_min50_sg0_i220_window5_size300',               #
+    'models/word2vec/w2v_mymodel_33_min1000_sg0_i200_window5_size300',             # ------------
+    'models/word2vec/w2v_mymodel_33_min5_sg0_i250_window3_size300_transSplit',     #
+    'models/word2vec/w2v_mymodel_33_min500_sg0_i220_window10_size300_transSplit',  # ------------
+    'models/word2vec/w2v_mymodel_33_mincount1_min1_sg0_i230_window5_size300',      #
+    'models/word2vec/w2v_mymodel_33_mincount1_min1_sg0_i400_window10_size300',     #
 ]
+
+# Общие требования: sg1 - не использовать
+# Скорость обучения и количество эпох (количество) - и скорость 0,005 пока лучший результат 220 достаточно
+
 
 bdd_rms = pd.read_excel('library/BDD.xlsx', names=['product',
                                                    'department',
@@ -30,18 +35,86 @@ bdd_rms['product'] = bdd_rms['product'].astype(str)
 
 
 model = Word2Vec.load(models_list[model_num])
-model_forecast = Word2Vec.load(models_list[3])
-
-#%%
+model_forecast = Word2Vec.load(models_list[5])
 
 app = Flask(__name__)
 CORS(app)
-#%%
+
 
 #%% API
 
+@app.route('/analogs/<string:product>/')
+def get_analogs(product):
 
-#  Принимает данные в виде таблицы DF: product, probability, mode_adeo, model_name, date_created, product_name, is_stm
+    #  Получаем датафрейм с аналогами
+    analogs_df_main = get_similar(str(product), num=5, same_model=True)
+
+    #  Сортируем и отбираем данные, возвращаем датафрейм
+    analogs_df_cut_sort = cut_and_sort(analogs_df_main,
+                                       min_products=1,
+                                       max_products=8,
+                                       sort_by_stm=True,
+                                       sort_by_date=False)
+
+    #  Конвертируем в формат json
+    analogs = convert_df_to_json(analogs_df_cut_sort)
+
+    return jsonify(analogs)
+
+
+@app.route('/complementary/<string:products>/')
+def get_complementary(products):
+
+    products_list = products.split(',')
+    main_product = products_list[0]
+
+    product_name = list(bdd_rms[bdd_rms['product'] == str(main_product)]['product_name'])[0]
+
+    #  Получаем датафрейм с аналогами
+    complementary_df_main = get_predicted(products_list, num_models=5, num_products=5)
+
+    #  Сортируем и отбираем данные, возвращаем датафрейм
+    complementary_df_cut_sort = cut_and_sort(complementary_df_main,
+                                       min_products=3,
+                                       max_products=5,
+                                       num_models=5,
+                                       sort_by_stm=False,
+                                       sort_by_date=False)
+
+    #  Конвертируем в формат json
+    complementary = convert_df_to_json(complementary_df_cut_sort)
+
+    complementary.update({'product': main_product})
+    complementary.update({'product_name': product_name})
+
+    return jsonify(complementary)
+
+
+@app.route('/supplementary/<string:products>/')
+def get_supplementary(products):
+
+    products_list = products.split(',')
+
+    #  Получаем датафрейм с аналогами
+    supplementary_df_main = get_supplement(products_list, num_models=5, num_products=5)
+
+    #  Сортируем и отбираем данные, возвращаем датафрейм
+    supplementary_df_cut_sort = cut_and_sort(supplementary_df_main,
+                                       min_products=3,
+                                       max_products=5,
+                                       num_models=5,
+                                       sort_by_stm=False,
+                                       sort_by_date=False)
+
+    #  Конвертируем в формат json
+    supplementary = convert_df_to_json(supplementary_df_cut_sort)
+
+    return jsonify(supplementary)
+
+
+#%%  Конвертировать в json
+
+
 def convert_df_to_json(df: pd.DataFrame):
     """Конвертировать DataFrame в формат json.
     Параметры:
@@ -96,106 +169,10 @@ def convert_df_to_json(df: pd.DataFrame):
 
     return result
 
-#%%
 
 
-@app.route('/analogs/<string:product>/')
-def get_analogs(product):
 
-    #  Получаем датафрейм с аналогами
-    analogs_df_main = get_similar(str(product), num=8, same_model=True)
-
-    #  Сортируем и отбираем данные, возвращаем датафрейм
-    analogs_df_cut_sort = cut_and_sort(analogs_df_main,
-                                       min_products=1,
-                                       max_products=8,
-                                       sort_by_stm=True,
-                                       sort_by_date=False)
-
-    #  Конвертируем в формат json
-    analogs = convert_df_to_json(analogs_df_cut_sort)
-
-    return jsonify(analogs)
-
-
-@app.route('/complementary/<string:products>/')
-def get_complementary(products):
-
-    start_time = datetime.now()
-
-    products_list = products.split(',')
-    main_product = products_list[0]
-
-    product_name = list(bdd_rms[bdd_rms['product'] == str(main_product)]['product_name'])[0]
-
-
-    # print('Получение названия артикула: {}'.format(datetime.now() - start_time))
-    # new_time = datetime.now()
-
-    #  Получаем датафрейм с аналогами
-    analogs_df_main = get_predicted(products_list, num_models=5, num_products=5)
-
-    # print('Составление DataFrame предсказания: {}'.format(datetime.now() - new_time))
-    # new_time = datetime.now()
-
-    #  Сортируем и отбираем данные, возвращаем датафрейм
-    analogs_df_cut_sort = cut_and_sort(analogs_df_main,
-                                       min_products=3,
-                                       max_products=5,
-                                       num_models=5,
-                                       sort_by_stm=False,
-                                       sort_by_date=False)
-
-    # print('Обрезать и отсортировать: {}'.format(datetime.now() - new_time))
-    # new_time = datetime.now()
-
-
-    #  Конвертируем в формат json
-    analogs = convert_df_to_json(analogs_df_cut_sort)
-
-    analogs.update({'product': main_product})
-    analogs.update({'product_name': product_name})
-
-    return jsonify(analogs)
-
-
-#%%
-
-# @app.route('/tools/<string:product>/')
-# def get_too(product):
-#     codes_list = product.split(',')
-#     names = []
-#     for p in codes_list:
-#         code = list(code_model_name[code_model_name['product'] == str(p)]['product'])[0]
-#         name = list(code_model_name[code_model_name['product'] == str(p)]['name'])[0]
-#         names.append([str(code), str(name)])
-#     similar_products = convert_df(get_tools(codes_list, num=600), min=1)
-#     return render_template(
-#         'tools.html', **locals())
-#
-#
-if __name__ == '__main__':
-    app.run()
-
-
-# @app.route('/forecast/<string:product>/')
-# def get_forec(product):
-#     codes_list = product.split(',')
-#     names = []
-#     for p in codes_list:
-#         code = list(code_model_name[code_model_name['product'] == str(p)]['product'])[0]
-#         name = list(code_model_name[code_model_name['product'] == str(p)]['name'])[0]
-#         names.append([str(code), str(name)])
-#     similar_products = convert_df(get_forecast(codes_list, num_codes=10, num_models=10))
-#     return render_template(
-#         'forecast.html', **locals())
-#
-#
-# if __name__ == '__main__':
-#     app.run()
-
-
-#%%
+#%% Обрезать и отсортировать
 
 def cut_and_sort(df: pd.DataFrame,
                  min_products=4,
@@ -285,7 +262,7 @@ def get_predicted(products: list, num_models=10, num_products=10, remove_used_mo
     start_time = datetime.now()
     analogs = pd.DataFrame(columns=['product', 'probability', 'model_adeo', 'name'])
 
-    predicted = model.predict_output_word(products, topn=num_products*num_models*2)
+    predicted = model.predict_output_word(products, topn=(len(products) + num_products)*num_models*2)
 
     print('Получено предсказание: {}'.format(datetime.now() - start_time))
     new_time = datetime.now()
@@ -304,7 +281,7 @@ def get_predicted(products: list, num_models=10, num_products=10, remove_used_mo
 
     for s in similars:
         for p in s:
-            pred = model.predict_output_word([str(p)], topn=num_products*num_models)
+            pred = model.predict_output_word([str(p)], topn=(len(products) + num_products)*num_models)
             pred = pd.DataFrame(pred, columns=['product', 'probability'])
             pred = pred[pred['product'] != p]
             predicted = predicted.append(pred)
@@ -355,8 +332,66 @@ def get_predicted(products: list, num_models=10, num_products=10, remove_used_mo
 
     return predicted
 
+#%% Возможно, клиент забыл купить
 
-#%%
+
+def get_supplement(products: list, num_models=5, num_products=3, remove_used_models=True):
+    """Получить список товаров, которые клиент, возможно, забыл приобрести.
+    Parameters:
+        products(list): Код продукта
+        num_products(int): Топ моделей
+        num_models(int): Топ артикулов в каждой модели
+        remove_used_models(bool): Не показывать модели, которые были в запросе
+    Возвращает:
+        predicted (list): список таблиц pd.DataFrame
+    """
+    analogs = pd.DataFrame(columns=['product', 'probability', 'model_adeo', 'name'])
+    predicted = pd.DataFrame(columns=['product', 'probability'])
+
+    # Получаем похожий товар для каждого товара в списке
+    similars = []
+    for product in products:
+        similars.append(list(analogs.append(get_similar(product, num=2, same_model=True))['product']))
+
+    # Добавляем товар в запросе
+    similars.append(products)
+
+    # Проходим по всем товарам циклом и определяем предсказание для каждого в отдельности
+    for s in similars:
+        for p in s:
+            pred = model.predict_output_word([str(p)], topn=(len(products) + num_products)*num_models)
+            pred = pd.DataFrame(pred, columns=['product', 'probability'])
+            pred = pred[pred['product'] != p]
+            predicted = predicted.append(pred)
+            print(p)
+        print(s)
+
+    predicted = predicted.sort_values(by='probability', ascending=False)
+
+    # Удаляем дубликаты артикулов
+    predicted = predicted.drop_duplicates(subset='product', keep='first')
+
+    # Удаляем позиции, которые в запросе
+    for product in products:
+        predicted = predicted[predicted['product'] != product]
+
+    # Подтягиваем модель и название
+    predicted = predicted.merge(bdd_rms, on='product')
+
+    # Удаляем использованные модели
+    if remove_used_models:
+        current_models = []
+        for product in products:
+            current_models.append(list(bdd_rms[bdd_rms['product'] == str(product)]['model_adeo'])[0])
+        for model_adeo in current_models:
+            predicted = predicted[predicted['model_adeo'] != model_adeo]
+
+    predicted = predicted.sort_values(by='probability', ascending=False)
+
+    return predicted
+
+
+#%% Прогноз будущих покупок
 
 def get_forecast(products: list, num_codes = 10, num_models = 10, remove_used_models = True):
     """Получить список похожих товаров.
@@ -398,8 +433,6 @@ def get_forecast(products: list, num_codes = 10, num_models = 10, remove_used_mo
 
     predicted = predicted
 
-
-
     # Удаляем позиции, которые в запросе
     for product in products:
         predicted = predicted[predicted['product'] != product]
@@ -411,7 +444,6 @@ def get_forecast(products: list, num_codes = 10, num_models = 10, remove_used_mo
         current_models = []
         for product in products:
             current_models.append(list(bdd_rms[bdd_rms['product'] == str(product)]['model_adeo'])[0])
-        print(current_models)
         for model_adeo in current_models:
             predicted = predicted[predicted['model_adeo'] != model_adeo]
 
@@ -419,61 +451,19 @@ def get_forecast(products: list, num_codes = 10, num_models = 10, remove_used_mo
 
     return predicted
 
-#%% Список инструментов и аксессуаров
-
-#
-# def get_tools(products: list, num=1000):
-#     """Получить список инструментов.
-#     Parameters:
-#         products(list): Список товаров
-#         num(int): Топ моделей
-#     Возвращает:
-#         predicted (list): список таблиц pd.DataFrame
-#     """
-#
-#     analogs = pd.DataFrame(columns=['product', 'probability', 'model_adeo', 'name'])
-#
-#     predicted = model_forecast.predict_output_word(products, topn=num)
-#     predicted = pd.DataFrame(predicted, columns=['product', 'probability'])
-#
-#     similars = []
-#     for product in products:
-#         similars.append(list(analogs.append(get_similar(product, num=4, same_model=True))['product']))
-#
-#     for s in similars:
-#         for p in s:
-#             pred = model_forecast.predict_output_word([str(p)], topn=4)
-#             pred = pd.DataFrame(pred, columns=['product', 'probability'])
-#             pred = pred[pred['product'] != p]
-#             predicted = predicted.append(pred)
-#
-#     predicted = predicted[predicted['product'].str.find("+") != -1]
-#
-#     predicted['product'] = predicted['product'].str.slice(0, 8)
-#
-#     predicted = predicted.sort_values(by='probability', ascending=False)
-#
-#     # Удаляем позиции, которые в запросе
-#     for product in products:
-#         predicted = predicted[predicted['product'] != product]
-#
-#     # Подтягиваем модель и название
-#     predicted = predicted.merge(code_model_name, on='product')
-#
-#     predicted = predicted.merge(tool_models, on='model_adeo')
-#
-#     predicted = predicted.iloc[:, 0:4]
-#
-#     #predicted = predicted[predicted['model_adeo'] == 'MOD_20']
-#
-#     predicted = predicted.sort_values(by='probability', ascending=False)
-#
-#     return predicted
 
 #%%
 
 
-# similar_products = get_similar(str('81946088'), num=50, same_model=True)
-# similar_products_conv = convert_df_to_json(similar_products)
+if __name__ == '__main__':
+    app.run()
 
 
+#%% Тест
+
+#print(get_similar('12317232', num=8, same_model=True))
+
+# for i in range(0, 6):
+#     model = Word2Vec.load(models_list[i])
+#     similars = get_similar('14154254', num=150, same_model=True)
+#     print(len(similars))
